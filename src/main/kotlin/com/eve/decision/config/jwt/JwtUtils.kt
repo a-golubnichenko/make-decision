493a3b5b66bd847d51b0a028e1f39c2f39cb6300
Artem Golubnichenko <artem@bigbrassband.com>
2022-05-13 17:47:12 +0300

    Fixed usage deprecated methods

--- src/main/kotlin/com/eve/decision/config/jwt/JwtUtils.kt
+++ src/main/kotlin/com/eve/decision/config/jwt/JwtUtils.kt
@@ -4,11 +4,14 @@ import com.eve.decision.service.auth.UserDetailsImpl
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.MalformedJwtException
 import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import java.security.Key
 import java.util.*
 
 
@@ -27,12 +30,15 @@ class JwtUtils {
         val userPrincipal = authentication.principal as UserDetailsImpl
         return Jwts.builder().setSubject(userPrincipal.username).setIssuedAt(Date())
                 .setExpiration(Date(Date().time + this.jwtExpirationMs!!))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret).compact()
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512).compact()
     }
 
     fun validateJwtToken(jwt: String?): Boolean {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwt)
+            Jwts.parserBuilder()
+                    .setSigningKey(getSigningKey())
+                    .build()
+                    .parseClaimsJws(jwt)
             return true
         } catch (e: MalformedJwtException) {
             logger.error("Can not validate token {}", e.message, e)
@@ -43,7 +49,15 @@ class JwtUtils {
     }
 
     fun getUserNameFromJwtToken(jwt: String?): String? {
-        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(jwt).body.subject
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(jwt).body.subject
+    }
+
+    private fun getSigningKey(): Key? {
+        val keyBytes: ByteArray = jwtSecret!!.toByteArray(StandardCharsets.UTF_8)
+        return Keys.hmacShaKeyFor(keyBytes)
     }
 
 }
