557e75dd71ae48d631732db70e27b423e9b70e64
Artem Golubnichenko <artem@bigbrassband.com>
2022-05-20 08:49:16 +0300

    Added options to question response

--- src/main/kotlin/com/eve/decision/dto/QuestionResponse.kt
+++ src/main/kotlin/com/eve/decision/dto/QuestionResponse.kt
@@ -6,6 +6,7 @@ data class QuestionResponse(
         val id: Long,
         val userId: Long,
         val text: String,
+        val options: Set<OptionResponse>,
         val createdAt: LocalDateTime,
         val updatedAt: LocalDateTime
 
--- src/main/kotlin/com/eve/decision/transformer/QuestionResponseTransformerExtension.kt
+++ src/main/kotlin/com/eve/decision/transformer/QuestionResponseTransformerExtension.kt
@@ -1,15 +1,25 @@
 package com.eve.decision.transformer
 
+import com.eve.decision.dto.OptionResponse
 import com.eve.decision.dto.QuestionResponse
+import com.eve.decision.model.Option
 import com.eve.decision.model.Question
+import com.eve.decision.model.Role
 import java.time.LocalDateTime
+import java.util.*
+import java.util.stream.Collectors
 
 fun Question?.toQuestionResponse(): QuestionResponse {
+
+    val options: MutableSet<OptionResponse> = this?.options?.stream()
+            ?.map { item: Option? -> item!!.toOptionResponse() }
+            ?.collect(Collectors.toSet()) ?: Collections.emptySet()
 
     return QuestionResponse(
             id = this?.id ?: 0L,
             userId = this?.userId ?: 0L,
             text = this?.text ?: "",
+            options = options,
             createdAt = this?.createdAt ?: LocalDateTime.now(),
             updatedAt = this?.updatedAt ?: LocalDateTime.now(),
     )
